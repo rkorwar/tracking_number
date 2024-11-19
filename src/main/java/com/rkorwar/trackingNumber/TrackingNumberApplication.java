5ce470bd190980671c505aad539db9c1e3d0222c
Rakesh Korwar <rakeshkorwar@gmail.com>
2024-11-19 13:02:47 +0530

    Changes for Swagger
    Changes for Swagger

--- src/main/java/com/rkorwar/trackingNumber/TrackingNumberApplication.java
+++ src/main/java/com/rkorwar/trackingNumber/TrackingNumberApplication.java
@@ -4,6 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Tracking Number API",
+				description = "API for generating and managing tracking numbers",
+				version = "v1",
+				contact = @Contact(
+						name = "Rakesh Korwar",
+						email = "********@gmail.com",
+						url = "https://www.*****.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "API for generating and managing tracking numbers"
+		)
+)
 public class TrackingNumberApplication {
 
 	public static void main(String[] args) {
